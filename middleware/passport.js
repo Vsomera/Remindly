9c1a4199dd7d2ade884c17adc948d19d9ecb5a62
Vilmor Somera <vsomera77@outlook.com>
2023-04-16 09:14:04 -0700

    cleaned up code + comments

--- middleware/passport.js
+++ middleware/passport.js
@@ -20,15 +20,16 @@ const localLogin = new LocalStrategy(
     }
 )
 
-
 passport.serializeUser((user, done) => {
   // creates a session for the user (stores info on the server abt the user)
   // executed by passport.js when done function is returned with a user
   // it then attached the user object in a var named (req.user)
     done(null, user.id);
   });
-  
+
 passport.deserializeUser((id, done) => {
+  // When the user makes a subsequent request, Passport retrieves the user's 
+  // session and calls deserializeUser to retrieve the user object associated with the session
     let user = userController.getUserById(id);  
     (user)
       ? done(null, user) 

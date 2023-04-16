c84e45b42682f116f2b8ef956fc205330ed1c5a2
Vilmor Somera <vsomera77@outlook.com>
2023-04-15 20:13:31 -0700

    better database format

--- database.js
+++ database.js
@@ -1,6 +1,25 @@
 let Database = {
     cindy: {
-        reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}]
+        reminders: [
+            {
+                id: 1, 
+                title: "Go Grocery Shopping", 
+                description: "Feb 8th at 1:30pm", 
+                completed: true
+            }, 
+            {
+                id: 2, 
+                title: "Dental Appointment", 
+                description: "March 10th at 2:30pm", 
+                completed: false
+            },
+            {
+                id: 3, 
+                title: "Final Exam", 
+                description: "April 8th at 2:30pm", 
+                completed: false
+            }
+        ]
     },
     alex: {
         reminders: []
